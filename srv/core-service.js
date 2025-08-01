5e0512a78497d845c0739b0cf27677f44b98dc79
prasad598 <prasad.bandaru7@gmail.com>
2025-08-02 01:28:58 +0530

    feat: include comments and attachments in TE_SR

---
+++ srv/core-service.js
@@ -0,0 +1,23 @@
+const cds = require('@sap/cds')
+const { SELECT } = cds
+
+module.exports = (srv) => {
+  const { CORE_COMMENTS, CORE_ATTACHMENTS } = srv.entities
+
+  srv.after('READ', 'TE_SR', async (results, req) => {
+    const db = srv.transaction(req)
+    const list = Array.isArray(results) ? results : [results]
+    await Promise.all(
+      list.map(async (item) => {
+        const key = item.REQ_TXN_ID
+        if (!key) return
+        item.CORE_COMMENTS = await db.run(
+          SELECT.from(CORE_COMMENTS).where({ REQ_TXN_ID: key })
+        )
+        item.CORE_ATTACHMENTS = await db.run(
+          SELECT.from(CORE_ATTACHMENTS).where({ REQ_TXN_ID: key })
+        )
+      })
+    )
+  })
+}
