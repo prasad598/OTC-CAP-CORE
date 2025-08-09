64d81ab6674c12ab7aec97606ccdb004f808d291
prasad598 <prasad.bandaru7@gmail.com>
2025-08-10 04:46:25 +0800

    Propagate user token for IAS profile lookup

--- srv/utils/ias.js
+++ srv/utils/ias.js
@@ -1,12 +1,14 @@
 const { executeHttpRequest } = require('@sap-cloud-sdk/http-client')
+const { retrieveJwt } = require('@sap-cloud-sdk/connectivity')
 
 async function fetchIasUser(req) {
   const userUuid = req?.user?.attr?.user_uuid || req?.user?.id
   if (!userUuid) {
     throw new Error('Missing user UUID')
   }
+  const jwt = retrieveJwt(req)
   const response = await executeHttpRequest(
-    { destinationName: 'CIS_SCIM_API' },
+    { destinationName: 'CIS_SCIM_API', jwt },
     {
       method: 'GET',
       url: `/scim/Users/${userUuid}`,
