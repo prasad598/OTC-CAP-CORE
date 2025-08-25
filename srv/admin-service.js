8c91540035b3d83e142bc38c0b8672b4ba2b515e
prasad598 <prasad.bandaru7@gmail.com>
2025-08-25 14:42:11 +0530

    Handle missing entities in purgeAllData

--- srv/admin-service.js
+++ srv/admin-service.js
@@ -16,16 +16,19 @@ module.exports = async function () {
 
   this.on('purgeAllData', async (req) => {
     const tx = cds.tx(req)
-    await Promise.all([
-      tx.run(DELETE.from(CORE_ATTACHMENTS)),
-      tx.run(DELETE.from(CORE_COMMENTS)),
-      tx.run(DELETE.from(CORE_USERS)),
-      tx.run(DELETE.from(AUTH_MATRIX)),
-      tx.run(DELETE.from(CONFIG_LDATA)),
-      tx.run(DELETE.from(MON_WF_PROCESS)),
-      tx.run(DELETE.from(MON_WF_TASK)),
-      tx.run(DELETE.from(TE_SR)),
-    ])
+    const entities = [
+      CORE_ATTACHMENTS,
+      CORE_COMMENTS,
+      CORE_USERS,
+      AUTH_MATRIX,
+      CONFIG_LDATA,
+      MON_WF_PROCESS,
+      MON_WF_TASK,
+      TE_SR,
+    ].filter(Boolean)
+
+    await Promise.all(entities.map((entity) => tx.run(DELETE.from(entity))))
+
     return { status: 'OK' }
   })
 }
